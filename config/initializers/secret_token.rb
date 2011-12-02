00e585f7b4d15be1c8eda4b56ac8620d368a5052
David Kormushoff <kormie@gmail.com>
2011-12-01 18:48:53 -0600

    Remove all rails files
    I'm moving to Sinatra so I'm going to remove all rails files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-KormushoffCom::Application.config.secret_token = 'df653e53d699ed3547fd1c96f179d8e90b94268d13f7bc87f164a9c21518c3c7bff5f12b440242d4a56d802ba44efb265e13d3aade3ac3799221f9ce984b2e24'
